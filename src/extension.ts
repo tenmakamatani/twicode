d8e948186250dccf41df116ffb31d676c5e2da4b
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-12-09 10:07:32 +0900

    Setup twitter client

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,26 @@
 import * as vscode from 'vscode';
 
+import Twitter from 'twitter';
+import { config as dotenvConfig } from 'dotenv';
+
+// Enable environment values
+dotenvConfig();
+const env = process.env;
+const {
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN_KEY,
+  ACCESS_TOKEN_SECRET
+} = env;
+
+// Setup twitter client
+const client = new Twitter({
+  consumer_key: CONSUMER_KEY ? CONSUMER_KEY : '',
+  consumer_secret: CONSUMER_SECRET ? CONSUMER_SECRET : '',
+  access_token_key: ACCESS_TOKEN_KEY ? ACCESS_TOKEN_KEY : '',
+  access_token_secret: ACCESS_TOKEN_SECRET ? ACCESS_TOKEN_SECRET : ''
+});
+
 export function activate(context: vscode.ExtensionContext) {
 
   console.log('Congratulations, your extension "twicode" is now active!');
